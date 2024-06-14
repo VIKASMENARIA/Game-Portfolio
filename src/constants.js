68d4beed5878a2fdcf6dbc8db018fce9db99798a
unknown <HP@D5-LAPTOP-W401.nmdc.co.in>
2024-06-14 12:45:38 +0530

    2D portfolio

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in Java,python ,c datascience with python ,machine learning these days.
+    I've made a couple of project  in that language. I also like java and Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/VIKASMENARIA" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+  Sometimes I'll have a snack or drink while I'm watching.`,
+  tv: `That's my TV. I've been watching youtubers a lot recently :`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `Here is my resume <a href="https://github.com/VIKASMENARIA?tab=repositories" target="_blank">Resume</a>`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. " target="_blank">here</a>.`,
+  library: `There are a lot of programming books on my shelves. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Vikas Menaria's portfolio, just close the tab.`,
+};
